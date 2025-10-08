373f9afc95f60ab5a0d61e90eaffeaa570ca8c4e
yashrajvrma <vermayashraj2005@gmail.com>
2025-10-09 03:27:55 +0530

    add headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,39 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div className="flex justify-center h-screen bg-foreground text-primary-foreground">
+      <div className="flex flex-col max-w-md w-full h-full border border-x mt-20 px-2">
+        {/* header and profile photo */}
+        <div className="flex justify-between">
+          <div className="flex flex-col justify-center">
+            <div className="text-lg">YASHRAJ VERMA</div>
+            <div className="text-sm text-secondary">@yashrajvrma</div>
+          </div>
+          <div className="border border-muted-foreground bg-accent p-1">
+            <Image
+              src="https://pbs.twimg.com/profile_images/1974875613837324288/ckIPGnPE_400x400.jpg"
+              alt="profile-img"
+              width={60}
+              height={60}
+            />
+          </div>
+        </div>
+        {/* link section  */}
+
+        <div></div>
+        {/* bio section */}
+        <div></div>
+        {/* skill section  */}
+        <div></div>
+
+        {/* work exp */}
+        <div></div>
+        {/* projects section */}
+        <div></div>
+        {/* footer section  */}
+        <div></div>
+      </div>
+    </div>
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://pbs.twimg.com/profile_images/1974875613837324288/ckIPGnPE_400x400.jpg"
+      ),
+    ],
+  },
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };

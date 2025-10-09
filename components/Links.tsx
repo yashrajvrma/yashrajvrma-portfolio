124fe4717177cbba0acd0330262f465b90780d72
yashrajvrma <vermayashraj2005@gmail.com>
2025-10-09 16:37:32 +0530

    fixed links

--- components/Links.tsx
+++ components/Links.tsx
@@ -11,50 +11,55 @@ export default function Links() {
   return (
     <div className="flex flex-col gap-y-2 mt-8">
       <div className="text-xs text-secondary">LINKS</div>
-      <div className="flex gap-x-4">
-        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary h-12 w-12">
+      <div className="flex justify-center items-center gap-x-3">
+        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary p-3">
           <Link
             href="https://drive.google.com/file/d/1FXuZi3ve1ZRpR6_DuLSlzfX7lRP8WOlt/view?usp=drivesdk"
             target="_blank"
-            className="hover:scale-125 transition duration-200 ease-in-out transform"
+            className="flex items-center gap-x-1 hover:scale-110 transition duration-200 ease-in-out transform"
           >
-            <ReadCvLogoIcon size={24} />
+            <ReadCvLogoIcon size={22} />
+            Resume
           </Link>
         </button>
-        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary h-12 w-12">
+        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary p-3">
           <Link
             href="mailto:yashrajv.work@gmail.com"
             target="_blank"
-            className="hover:scale-125 transition duration-200 ease-in-out transform"
+            className="flex items-center gap-x-1 hover:scale-110 transition duration-200 ease-in-out transform"
           >
-            <EnvelopeSimpleIcon size={24} />
+            <EnvelopeSimpleIcon size={22} />
+            Mail
           </Link>
         </button>
-        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary h-12 w-12">
+        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary p-3">
           <Link
             href="https://github.com/yashrajvrma"
             target="_blank"
-            className="hover:scale-125 transition duration-200 ease-in-out transform"
+            className="flex items-center gap-x-1 hover:scale-110 transition duration-200 ease-in-out transform"
           >
-            <GithubLogoIcon size={24} />
+            <GithubLogoIcon size={22} />
+            Github
           </Link>
         </button>
-        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary h-12 w-12">
+        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary p-3">
           <Link
             href="https://x.com/yashrajvrma"
             target="_blank"
-            className="hover:scale-125 transition duration-200 ease-in-out transform"
+            className="flex items-center gap-x-1 hover:scale-110 transition duration-200 ease-in-out transform"
           >
-            <XLogoIcon size={24} />
+            <XLogoIcon size={22} />
+            Twitter
           </Link>
         </button>
-        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary h-12 w-12">
+        <button className="flex justify-center items-center rounded-none hover:bg-muted-foreground text-secondary p-3">
           <Link
             href="https://www.linkedin.com/in/theyashrajverma/"
             target="_blank"
-            className="hover:scale-125 transition duration-200 ease-in-out transform"
+            className="flex items-center gap-x-1 hover:scale-110 transition duration-200 ease-in-out transform"
           >
-            <LinkedinLogoIcon size={24} />
+            <LinkedinLogoIcon size={22} />
+            Linkedin
           </Link>
         </button>
       </div>

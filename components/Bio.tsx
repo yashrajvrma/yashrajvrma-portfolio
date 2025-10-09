fd0821ca710ec7bc680828718d9f1decf9bf7f58
yashrajvrma <vermayashraj2005@gmail.com>
2025-10-09 16:29:58 +0530

    added bio

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
+
 export default function Bio() {
   return (
     <div className="flex flex-col pt-5 text-secondary">
       <div>20 ★ Engineer ★ FullStack Dev</div>
+      <br />
       <div>
-        Final year student pursuing B.E in Electronics and Telecommunication
-        Engineering
+        Hi there, I'm Yashraj a final year undergrad{" "}
+        <span>
+          <Link
+            className="hover:underline font-semibold text-blue-400 hover:text-blue-500"
+            href="https://www.tcetmumbai.in/"
+            target="_blank"
+          >
+            @TCET
+          </Link>
+        </span>{" "}
+        pursuing Engineering. I love to build cool products that makes peoples
+        live easier. I play, read books and create content for fun.
       </div>
     </div>
   );

83e71792acdb47cb16e69967aa10bbf898e2a09d
yashrajvrma <vermayashraj2005@gmail.com>
2025-10-19 12:51:21 +0530

    fixed margin

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="flex justify-center bg-foreground text-primary-foreground text-sm min-h-screen relative px-2">
       <div className="absolute top-20 left-0 right-0 border-b-1 border-border border-dashed w-full"></div>
       <div className="relative border-x-1 border-border border-dashed sm:max-w-xl max-w-sm min-h-screen h-full">
-        <div className="flex flex-col sm:max-w-xl max-w-sm w-full h-full sm:px-2 px-4 mt-20 ">
+        <div className="flex flex-col sm:max-w-xl max-w-sm w-full h-full sm:px-4 px-4 mt-20 ">
           <Header />
           <Bio />
           <Links />
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="flex py-2 gap-x-2">
+    <div className="flex py-4 gap-x-2">
       <div className="border-2 border-border h-[60px] w-[60px]">
         <Image
           src="https://pbs.twimg.com/profile_images/1974875613837324288/ckIPGnPE_400x400.jpg"

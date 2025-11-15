ea3d89728886baa93d240207c0c7f41e1b853aab
yashrajvrma <vermayashraj2005@gmail.com>
2025-11-16 01:33:35 +0530

    refactor: remove all unused components and non functioning button in headrs

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,20 @@
-import { HeartIcon } from "@phosphor-icons/react/dist/ssr";
-import { ChartNoAxesColumn } from "lucide-react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import {
-  ButtonGroup,
-  ButtonGroupSeparator,
-} from "@/components/ui/button-group";
+import profileImg from "../public/assets/images/x-profile.jpg";
 
 export default function Header() {
   return (
     <div className="flex py-4 gap-x-2">
-      <div className="border-2 border-border h-[90px] w-[90px]">
-        <Image
-          src="https://pbs.twimg.com/profile_images/1974875613837324288/ckIPGnPE_400x400.jpg"
-          alt="profile-img"
-          width={60}
-          height={60}
-          className="w-full h-full"
-        />
-      </div>
-      <div className="flex flex-col justify-center">
-        <div className="text-lg font-semibold">
-          YASHRAJ VERMA
-          <span className="pl-2 text-sm text-secondary/85">(he/him)</span>
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col justify-center">
+          <div className="text-lg font-semibold">
+            YASHRAJ VERMA
+            <span className="pl-2 text-sm text-secondary/85">(he/him)</span>
+          </div>
+          <div className="text-sm text-secondary/60">@yashrajvrma</div>
         </div>
-        <div className="text-sm text-secondary/60">@yashrajvrma</div>
+
         {/* like and views component component */}
-        <div className="flex gap-x-4 pt-2 text-sm">
+        {/* <div className="flex gap-x-4 pt-2 text-sm">
           <div className="flex justify-center items-center gap-x-2">
             <ButtonGroup>
               <Button variant="outline">Button</Button>
@@ -41,6 +29,16 @@ export default function Header() {
             200
           </div>
           <div className="text-primary-foreground">ii</div>
+        </div> */}
+        <div className="border-2 border-border h-[80px] w-[80px]">
+          <Image
+            src={profileImg}
+            alt="profile-img"
+            // width={60}
+            // height={60}
+            className="w-full h-full"
+            quality={100}
+          />
         </div>
       </div>
     </div>
